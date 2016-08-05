22d001a830c849022d6cee91ec33017d92c25cb4
Roy Cohen <roee78@users.noreply.github.com>
2016-08-04 21:59:30 -0700

    NA

---
+++ validAnagram.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public boolean isAnagram(String s, String t) {
+        
+        if (s.length() != t.length()) {
+            return false;
+        }   
+        
+        int[] alpha = new int[26];
+        
+        for (int i = 0; i < s.length(); i++){
+            alpha[s.charAt(i) - 'a']++;
+            alpha[t.charAt(i) - 'a']--;
+        }
+        
+        for (int count : alpha){
+            if (count != 0){
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
